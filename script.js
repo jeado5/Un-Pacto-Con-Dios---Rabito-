5f3df0beaea54c54a104825d15efc1053b615f4e
jeado5 <145814273+jeado5@users.noreply.github.com>
2023-09-26 11:30:59 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function mostrarImagen() {
+    document.getElementById('image').style.display = 'block';
+    document.getElementById('video').style.display = 'none';
+}
+
+function mostrarVideo() {
+    document.getElementById('image').style.display = 'none';
+    document.getElementById('video').style.display = 'block';
+}
